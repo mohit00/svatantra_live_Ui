adcd702984cb4b28c892bf46181aab6d4b251fb1
developer-ting <108664408+developer-ting@users.noreply.github.com>
2024-12-20 14:22:42 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+// MODULES //
+
+// COMPONENTS //
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import MetaTags from "@/components/MetaTags";
+
+// SECTIONS //
+
+// PLUGINS //
+
+// UTILS //
+
+// STYLES //
+import styles from "@/styles/pages/Home.module.scss";
+
+// IMAGES //
+
+// DATA //
+
+/** Home Page */
+export default function HomePage() {
+	return (
+		<div>
+			{/* Metatags */}
+			<MetaTags Title={"Home"} Desc={"Home Desc"} OgImg={""} Url={"/"} />
+
+			{/* Header */}
+			<Header />
+
+			{/* Page Content starts here */}
+			<main className={`${styles.HomePage} bg_primary`}>
+				<h2>This is ting base next.js template</h2>
+			</main>
+			{/* Page Content ends here */}
+
+			{/* Footer */}
+			<Footer />
+		</div>
+	);
+}
