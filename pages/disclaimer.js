00fe4dbe9952d892331627bd77e52a35a60e785a
kevinTingGit <kevin.anthony@ting.in>
2025-05-06 18:03:35 +0530

    .

--- pages/disclaimer.js
+++ pages/disclaimer.js
@@ -35,14 +35,14 @@ export default function DisclaimerPage() {
 					<Breadcrum linknest1={"/disclaimer"} linknestTitle1={"Disclaimer"} />
 
 					<h1 className="section_title pb_40 text_center">Disclaimer</h1>
-					<p className="f_w_r text_center pb_20">
+					<p className="f_w_r  pb_20">
 						This is the official site of Svatantra Microfin Private Limited
 						(Svatantra) and provides general information about Svatantra. All the
 						text, graphics, trademarks and all other content displayed on this site
 						are owned by Svatantra. The information and content is based on the laws
 						applicable in India.
 					</p>
-					<p className="f_w_r text_center pb_20">
+					<p className="f_w_r  pb_20">
 						While efforts are made to keep the information on this site accurate and
 						timely, Svatantra does not guarantee or endorse the content, accuracy or
 						completeness of the information, text, graphics, hyperlinks and other
@@ -52,7 +52,7 @@ export default function DisclaimerPage() {
 						provided &quot;as is&quot; without representations or warranties of any
 						kind, whether express or implied.
 					</p>
-					<p className="f_w_r text_center pb_20">
+					<p className="f_w_r  pb_20">
 						To the fullest extent permissible, Svatantra. disclaims any warranties,
 						express and / or implied, including but not limited to implied warranties
 						of merchantability, fitness for a particular purpose, non-infringement,
@@ -65,13 +65,13 @@ export default function DisclaimerPage() {
 						materials in the site in terms of their completeness, correctness,
 						accuracy, adequacy, usefulness, timeliness, reliability or otherwise.
 					</p>
-					<p className="f_w_r text_center pb_20">
+					<p className="f_w_r  pb_20">
 						Svatantra is not responsible for any special, indirect, incidental or
 						consequential damages that may arise from the use of or the inability to
 						use, the site and / or the materials contained on the site irrespective of
 						whether the materials contained on the site are provided by Svatantra.
 					</p>
-					<p className="f_w_r text_center pb_20">
+					<p className="f_w_r  pb_20">
 						The contents of this site are subject to change without notice and
 						Svatantra reserves the right to terminate access to this web site at any
 						time and without notice. Entire contents of the site including all
@@ -84,7 +84,7 @@ export default function DisclaimerPage() {
 						incorporated in any other work, publication or site, whether in hardcopy
 						or electronic format, including postings to any other site.
 					</p>
-					<p className="f_w_r text_center pb_20">
+					<p className="f_w_r  pb_20">
 						This site may contain some hyperlinks which lead out of this site.
 						Information contained in any site linked from this site has not been
 						reviewed for accuracy or legal sufficiency. Svatantra is not responsible
@@ -92,21 +92,21 @@ export default function DisclaimerPage() {
 						external links should not be construed as an endorsement of the links or
 						their content.
 					</p>
-					<p className="f_w_r text_center pb_20">
+					<p className="f_w_r  pb_20">
 						No information on this site shall constitute an invitation to invest in
 						neither Svatantra nor its respective officers, employees or agents shall
 						be liable for any loss, damage of expense arising out of any access to or
 						use of this site or any site linked to it, including, without limitation,
 						any loss of profit, indirect, incidental or consequential loss
 					</p>
-					<p className="f_w_r text_center pb_20">
+					<p className="f_w_r  pb_20">
 						If Svatantra becomes aware of inappropriate use of the web site or any of
 						its contents, Svatantra will respond in any way that, in its sole
 						discretion, Svatantra deems appropriate. Svatantra may report to law
 						enforcement authorities any actions that may be considered illegal, as
 						well as any information it receives of such illegal conduct.
 					</p>
-					<p className="f_w_r text_center pb_60">
+					<p className="f_w_r  pb_60">
 						By viewing this site you are deemed to agree to jurisdiction of the courts
 						at Mumbai, India, in respect of any action arising therefrom or related
 						thereto.
